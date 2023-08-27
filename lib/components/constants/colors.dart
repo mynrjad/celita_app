969fad50fac416a79d77f46439fbc67388f846e5
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 03:02:11 +0800

    Added image assets and color theme.

--- lib/components/constants/colors.dart
+++ lib/components/constants/colors.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
 
+//This is a list of all CEpp Application colors.
+const CEppPrimaryColor = Color(0xFFEFC818);
+const CEppSecondaryColor = Color(0xFFFF6157);
+const CEppAccentColor = Color(0xFF475AFF);
+
+//Onboarding colors.
+const CEppOnboardingColor = Colors.white;
