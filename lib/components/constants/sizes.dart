a8a7deb56b23073082bee2283bcdd56adbf8d270
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 03:32:58 +0800

    Added sizes and text strings for call.

---
+++ lib/components/constants/sizes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+//This is the defautl sizing of the application.
+const CEppDefaultSize = 30.0;
+const CEppSplashSize = 30.0;
+const CEppButtonSize = 30.0;
---
+++ lib/components/constants/text_strings.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//This is a list of text needed on CEpp screens.
+//Onboarding
+
+//1
+const CEppOnboardingHeading1 = "NAKAKA-ENGGANYONG";
+const CEppOnboardingSubheading1 = "PAGSALIN";
+const CEppOnboardingSubtitle1 =
+    "Sanayin ang sarili sa tunog at pagkakabigkas ng dialekto na makatutulong sa kanila upang maatim ang natural at pananalita";
+
+//2
+const CEppOnboardingHeading2 = "MAGKAISA AT ";
+const CEppOnboardingSubheading2 = "GUMALING";
+const CEppOnboardingSubtitle2 =
+    "Paliitin ang agwat ng komunikasyon at magkaisa. Alamin at sanayin ang sarili sa pagsasalita ng Tagalog-Cebuano";
+
+//Welcome Screen
+const CEppWelcomeHeading1 = "TARA NA'T MAGSALITA";
+const CEppWelcomeSubtitle1 = "Maligayang pagdating sa Celita";
