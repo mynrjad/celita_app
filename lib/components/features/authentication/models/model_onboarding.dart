c01dfe45844be45efcc7f51a3d928c501b835453
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 03:42:59 +0800

    Added model for onboarding screen.

---
+++ lib/components/features/authentication/models/model_onboarding.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class OnBoardingModel {
+  final String image;
+  final String heading;
+  final String subheading;
+  final String subtitle;
+
+  OnBoardingModel ({
+    required this.image,
+    required this.heading,
+    required this.subheading,
+    required this.subtitle,
+});
+}
