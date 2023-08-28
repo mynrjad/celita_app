b48538071bf3e1c1cbc7d618e100678026f27db0
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 14:46:32 +0800

    Added images on Onboarding Screen.

--- lib/components/constants/image_strings.dart
+++ lib/components/constants/image_strings.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 //This is a list of CEpp Image assets.
 //Onboarding Screens
-const String CEppOnboarding1 = "assets/onboarding_assets/onboarding_one";
-const String CEppOnboarding2 = "assets/onboarding_assets/onboarding_two";
+const String CEppOnboarding1 = "assets/onboarding_assets/onboarding_one.png";
+const String CEppOnboarding2 = "assets/onboarding_assets/onboarding_two.png";
 
 //Welcome Screen
-const String CEppWelcome1 = "assets/welcome_assets/welcome_one";
+const String CEppWelcome1 = "assets/welcome_assets/welcome_one.png";
 
--- lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
+++ lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
@@ -1,6 +1,8 @@
 import 'package:celita/components/constants/colors.dart';
+import 'package:celita/components/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import '../../../../constants/text_strings.dart';
  
 class CEppOnboardingScreen extends StatelessWidget {
   const CEppOnboardingScreen({super.key});
@@ -12,7 +14,15 @@ class CEppOnboardingScreen extends StatelessWidget {
         children: [
           LiquidSwipe(
               pages: [
-                Container(color: CEppOnboardingColor),
+                Container(color: CEppOnboardingColor,
+                child: Column(
+                  children: [
+                    Image(image: AssetImage(CEppOnboarding1)),
+                    Text(CEppOnboardingHeading1),
+                    Text(CEppOnboardingSubheading1),
+                    Text(CEppOnboardingSubtitle1),
+                  ],
+                )),
                 Container(color: CEppPrimaryColor),
                 Container(color: CEppSecondaryColor),
           ],
