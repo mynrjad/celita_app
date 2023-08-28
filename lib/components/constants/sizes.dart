4a0a69f89d420e7909711db00fa265f8b1737990
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 15:31:16 +0800

    Added onboarding screen widget.

--- lib/components/constants/sizes.dart
+++ lib/components/constants/sizes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 //This is the defautl sizing of the application.
-const CEppDefaultSize = 30.0;
-const CEppSplashSize = 30.0;
+const CEppDefaultSize = 40.0;
+const CEppSplashSize = 40.0;
 const CEppButtonSize = 30.0;
--- lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
+++ lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
@@ -1,36 +1,64 @@
 import 'package:celita/components/constants/colors.dart';
 import 'package:celita/components/constants/image_strings.dart';
+import 'package:celita/components/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import '../../../../constants/text_strings.dart';
- 
+
 class CEppOnboardingScreen extends StatelessWidget {
   const CEppOnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Stack (
-        children: [
-          LiquidSwipe(
-              pages: [
-                Container(color: CEppOnboardingColor,
-                child: Column(
-                  children: [
-                    Image(image: AssetImage(CEppOnboarding1)),
-                    Text(CEppOnboardingHeading1),
-                    Text(CEppOnboardingSubheading1),
-                    Text(CEppOnboardingSubtitle1),
-                  ],
-                )),
-                Container(color: CEppPrimaryColor),
-                Container(color: CEppSecondaryColor),
+        body: Stack(
+      children: [
+        LiquidSwipe(
+          pages: [
+            OnboardingPage(size: size),
+            Container(color: CEppPrimaryColor),
+            Container(color: CEppSecondaryColor),
           ],
-          ),
-        ],
-      )
-    );
+        ),
+      ],
+    ));
   }
 }
 
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({
+    super.key,
+    required this.size,
+  });
 
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(CEppDefaultSize),
+        color: CEppOnboardingColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(image: AssetImage(CEppOnboarding1),
+            height: size.height * 0.4,),
+            Column(
+              children: [
+                Text(CEppOnboardingHeading1,
+                style: Theme.of(context).textTheme.displayMedium),
+                Text(CEppOnboardingSubheading1,
+                    style: Theme.of(context).textTheme.displayLarge),
+                Text(CEppOnboardingSubtitle1,
+                  style: Theme.of(context).textTheme.displaySmall,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 100.0,),
+              ],
+            ),
+          ],
+        ));
+  }
+}
