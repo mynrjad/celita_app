c4d35d0671580dd6a0c06dec1e3cd96ef2d189ed
mynrjad <ryemjian.dumlao@gmail.com>
2023-08-28 17:04:40 +0800

    Added buttons and active index on Onboarding screen.

--- lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
+++ lib/components/features/authentication/screens/onboarding screen/onboarding_screen.dart
@@ -4,6 +4,7 @@ import 'package:celita/components/constants/sizes.dart';
 import 'package:celita/components/features/authentication/models/model_onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../../../../constants/text_strings.dart';
 import 'onboarding_page_widget.dart';
 
@@ -26,34 +27,65 @@ class CEppOnboardingScreen extends StatelessWidget {
 
       OnboardingPage(
           model: OnBoardingModel(
-             image: CEppOnboarding2,
-             heading: CEppOnboardingHeading2,
-             subheading: CEppOnboardingSubheading2,
-             subtitle: CEppOnboardingSubtitle2,
-             size: size.height,
-             bgColor: CEppOnboardingColor,
+        image: CEppOnboarding2,
+        heading: CEppOnboardingHeading2,
+        subheading: CEppOnboardingSubheading2,
+        subtitle: CEppOnboardingSubtitle2,
+        size: size.height,
+        bgColor: CEppOnboardingColor,
       )),
     ];
+    final controller = LiquidController();
 
     return Scaffold(
         body: Stack(
-          alignment: Alignment.center,
+      alignment: Alignment.center,
       children: [
-        LiquidSwipe(pages: pages),
+        LiquidSwipe(
+          pages: pages,
+          liquidController: controller,
+        ),
+
+        //Button
         Positioned(
-          bottom:60.0,
-          child: OutlinedButton(onPressed: () {  },
+          bottom: 60.0,
+          child: OutlinedButton(
+            onPressed: () {},
             style: ElevatedButton.styleFrom(
               shape: const CircleBorder(),
               foregroundColor: Colors.white,
-            ), child: Container(
+            ),
+            child: Container(
               padding: const EdgeInsets.all(40),
               decoration: const BoxDecoration(
-                color: CEppSecondaryColor, shape: BoxShape.circle,
+                color: CEppSecondaryColor,
+                shape: BoxShape.circle,
               ),
               child: Icon(Icons.arrow_forward_ios),
-            ),),
-        )
+            ),
+          ),
+        ),
+        //Skip
+        Positioned(
+          top: 40,
+          right: 20,
+          child: TextButton(
+            onPressed: () {},
+            child: const Text("Skip", style: TextStyle(color: Colors.black87)),
+          ),
+        ),
+        //Active Index
+        Positioned(
+          top: 60,
+          child: AnimatedSmoothIndicator(
+            count: 2,
+            activeIndex: controller.currentPage,
+            effect: WormEffect(
+              activeDotColor: Color(0xFFFF6157),
+              dotHeight: 5.0,
+            ),
+          )
+        ),
       ],
     ));
   }
