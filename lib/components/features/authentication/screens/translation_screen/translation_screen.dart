2ba11c22390a77f815ddf3b19bbdcc39cb830edb
GeneralFinny <143330234+GeneralFinny@users.noreply.github.com>
2023-10-03 21:46:33 +0800

    Update translation_screen.dart
    copy pasted my code

--- lib/components/features/authentication/screens/translation_screen/translation_screen.dart
+++ lib/components/features/authentication/screens/translation_screen/translation_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class TranslatorScreen extends StatelessWidget {
+class TranslatorScreen extends StatefulWidget {
+  @override
+  _TranslatorScreenState createState() => _TranslatorScreenState();
+}
+
+class _TranslatorScreenState extends State<TranslatorScreen> {
+  TextEditingController _textEditingController = TextEditingController();
+  String _translatedText = "";
+
+  void translateText() async {
+    String inputText = _textEditingController.text;
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:3000/translate'), // Update with your Flask API endpoint
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'input_text': inputText}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          _translatedText = data['translated_text'];
+        });
+      } else {
+        setState(() {
+          _translatedText = "Translation failed"; // Handle error here
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _translatedText = "Translation failed"; // Handle error here
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Celita'),
-        backgroundColor: Color(0xFFFFBC57), // Set the color to FFBC57
+        backgroundColor: Color(0xFFFFBC57),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -19,27 +56,32 @@ class TranslatorScreen extends StatelessWidget {
               alignment: Alignment.center,
               color: Colors.grey.shade200,
               child: Text(
-                'Translation Box',
+                _translatedText, // Display the translated text here
                 style: TextStyle(fontSize: 18),
               ),
             ),
           ),
-          // Translated Text Box
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              alignment: Alignment.center,
-              color: Colors.grey.shade300,
-              child: Text(
-                'Translated Text Box',
-                style: TextStyle(fontSize: 18),
+          // Text Input Field
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                hintText: 'Enter text to translate',
               ),
             ),
+          ),
+          // Translate Button
+          ElevatedButton(
+            onPressed: () {
+              translateText();
+            },
+            child: Text('Translate'),
           ),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Color(0xFFFFBC57), // Set the color to FFBC57
+        color: Color(0xFFFFBC57),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -65,5 +107,11 @@ class TranslatorScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
