e33a0eabf48f735af365b8d0d3e9e4019c9fe8bd
GeneralFinny <143330234+GeneralFinny@users.noreply.github.com>
2023-10-28 11:19:25 +0800

    Update translation_screen.dart

--- lib/components/features/authentication/screens/translation_screen/translation_screen.dart
+++ lib/components/features/authentication/screens/translation_screen/translation_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:google_speech/speech_client_authenticator.dart';
+import 'package:google_speech/google_speech.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+import 'dart:io';
+import 'dart:async';
 import '../../../../constants/colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,104 +17,159 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final SpeechToText _speechToText = SpeechToText();
+  
 
-  bool _speechEnabled = false;
-  String _wordsSpoken = "";
-  double _confidenceLevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'poppins',
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool is_Transcribing = false;
+  String content = '';
+
+  void transcribe() async {
+    setState(() {
+      is_Transcribing = true;
+    });
+    final serviceAccount = ServiceAccount.fromString(
+        '${(await rootBundle.loadString('assets/androdrop-12345-c0f399677f1e.json'))}');
+    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
+
+    final config = RecognitionConfig(
+        encoding: AudioEncoding.LINEAR16,
+        model: RecognitionModel.basic,
+        enableAutomaticPunctuation: true,
+        sampleRateHertz: 16000,
+        languageCode: 'en-US');
+
+    final audio = await _getAudioContent('test.wav');
+    await speechToText.recognize(config, audio).then((value) {
+      setState(() {
+        content = value.results.map((e) => e.alternatives.first.transcript).join('\n');
+      });
+    }).whenComplete(() {
+      setState(() {
+        is_Transcribing = false;
+      });
+    });
+  }
+
+  Future<List<int>> _getAudioContent(String name) async {
+    //final directory = await getApplicationDocumentsDirectory();
+    //final path = directory.path + '/$name';
+    final path = '/sdcard/Download/temp.wav';
+    return File(path).readAsBytesSync().toList();
+  }
 
   @override
   void initState() {
+    setPermissions();
     super.initState();
-    initSpeech();
   }
 
-  void initSpeech() async {
-    _speechEnabled = await _speechToText.initialize();
-    setState(() {});
-  }
-
-  void _startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult);
-    setState(() {
-      _confidenceLevel = 0;
-    });
-  }
-
-  void _stopListening() async {
-    await _speechToText.stop();
-    setState(() {});
-  }
-
-  void _onSpeechResult(result) {
-    setState(() {
-      _wordsSpoken = "${result.recognizedWords}";
-      _confidenceLevel = result.confidence;
-    });
+  void setPermissions() async {
+    await Permission.manageExternalStorage.request();
+    await Permission.storage.request();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 108, 96, 225),
       appBar: AppBar(
-        backgroundColor: CEppPrimaryColor,
-        title: Text(
-          'Celita',
-          style: TextStyle(
+        toolbarHeight: 80,
+        backgroundColor: Color.fromARGB(255, 108, 96, 225),
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Transcribe Your Audio'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
             color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(50),
+              topLeft: Radius.circular(50),
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.all(5.0),
+                  child: content == ''
+                      ? Text(
+                    'Your text will appear here',
+                    style: TextStyle(color: Colors.grey),
+                  )
+                      : Text(
+                    content,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: is_Transcribing
+                      ? Expanded(
+                    child: LoadingIndicator(
+                      indicatorType: Indicator.ballPulse,
+                      colors: [Colors.red, Colors.green, Colors.blue],
+                    ),
+                  )
+                      : ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 10,
+                      primary: Color.fromARGB(255, 108, 96, 225),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    onPressed: is_Transcribing ? () {} : transcribe,
+                    child: is_Transcribing
+                        ? CircularProgressIndicator()
+                        : Text(
+                      'Transcribe',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                _speechToText.isListening
-                    ? "Nakikinig..."
-                    : _speechEnabled
-                    ? "Pindutin at Simulan Magsalita"
-                    : "Walang Speech_to_Text Recognition",
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  _wordsSpoken,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ),
-            ),
-            if (_speechToText.isNotListening && _confidenceLevel > 0)
-              Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 100,
-                ),
-                child: Text(
-                  "Confidence: ${(_confidenceLevel * 100).toStringAsFixed(1)}%",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w200,
-                  ),
-                ),
-              )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _speechToText.isListening ? _stopListening : _startListening,
-        tooltip: 'Listen',
-        child: Icon(
-          _speechToText.isNotListening ? Icons.mic_off : Icons.mic,
-          color: Colors.white,
-        ),
-        backgroundColor: CEppPrimaryColor,
       ),
     );
   }
